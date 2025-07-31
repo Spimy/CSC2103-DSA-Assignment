233dbe55aba1e98420c659775be0a111e52d2890
Spimy <williamlaw.3001@gmail.com>
2025-07-31 15:07:35 +0800

    👌 IMPROVE: Formatting

--- src/tasks/problem_1.py
+++ src/tasks/problem_1.py
@@ -1,7 +1,9 @@
 class MergeSort:
     # We need to merge two sorted halves of the arr into a single sorted segment
     def merge(self, arr: list[int], left: int, mid: int, right: int) -> None:
-        print(f"\nMerging left: {arr[left:mid + 1]} and right: {arr[mid + 1:right + 1]}")
+        print(
+            f"\nMerging left: {arr[left:mid + 1]} and right: {arr[mid + 1:right + 1]}"
+        )
         n1 = mid - left + 1  # n1 is the size of the left subarray
         n2 = right - mid  # n2 is the size of the right subarray
 
@@ -59,14 +61,17 @@ class MergeSort:
             print(f"  Right: {arr[mid + 1:right + 1]}")
 
             self.merge_sort(arr, left, mid)  # Now we recursively sort the left half.
-            self.merge_sort(arr, mid + 1, right)  # Then we recursively sort the right half
+            self.merge_sort(
+                arr, mid + 1, right
+            )  # Then we recursively sort the right half
             self.merge(arr, left, mid, right)  # Then we merge it into a sorted array
 
     # this method is to create a copy instead of mutating the original
     def sort_copy(self, arr: list[int]) -> list[int]:
-        arr_copy = arr[:] # Manually copy from the original
+        arr_copy = arr[:]  # Manually copy from the original
         self.merge_sort(arr_copy, 0, len(arr_copy) - 1)
         return arr_copy
+
 
 def problem_1() -> None:
     sorter = MergeSort()
@@ -75,7 +80,7 @@ def problem_1() -> None:
         # Now we ask for the user input then put it into the mergeSort function
         print("Please enter some numbers \n(separate them by spaces. i.e. 9 10 71 3)")
         user_input = input()
-        if not user_input.strip(): # check if the user enters an empty input
+        if not user_input.strip():  # check if the user enters an empty input
             print("No input detected. Please enter at least one number.")
             continue
         try:
@@ -90,12 +95,10 @@ def problem_1() -> None:
 
     # Now we call mergeSort() to sort the user input
     print(f"\nInitial array: {arr}")
-    sorted_arr = sorter.sort_copy(arr) # Make a copy and sort that
+    sorted_arr = sorter.sort_copy(arr)  # Make a copy and sort that
 
     # Now we print the sorted array
     print("Final sorted array:")
     for i in sorted_arr:
         print(i, end=" ")
     print()
-
-
